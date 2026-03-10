b3e1a57d3fcf77c0e30e94cb47a8e00617ed5524
NYKSSilva <nicollysantossilva0508@gmail.com>
2026-03-10 11:02:46 -0300

    começando o app

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.css';
+import {TimerIcon} from './styles.module.css'
+export function Logo(){
+   return (
+    <div>
+        <a href="#"><TimerIcon/><span>Teste</span></a>
+    </div>
+   )
+}
