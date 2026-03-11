ba573e579fc8a576dc8896f8f420fb6a8d5bdb97
NYKSSilva <nicollysantossilva0508@gmail.com>
2026-03-11 11:17:09 -0300

    forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
 import './styles/global.css'
 import './styles/theme.css'
-import {Heading} from './components/Heading';
-import {Container} from './components/container'
-import {Logo} from './components/Logo'
-import {Menu} from './components/Menu'
+import { Heading } from './components/Heading';
+import { Container } from './components/container'
+import { Logo } from './components/Logo'
+import { Menu } from './components/Menu'
+import { CountDown } from './components/CountDown'
+import {Formulario} from './components/Formulario'
 export function App() {
   return (
     <>
-    <Container>
-      <Logo/>
-    </Container>
+      <Container>
+        <Logo />
+      </Container>
 
-    <Container>
-      <Menu/>
-    </Container>
+      <Container>
+        <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
+      </Container>
+
+      <Container>
+       <Formulario/>
+      </Container>
 
       <Container>
         <Heading>Logo</Heading>
       </Container>
-      
+
       <Container>
         <Heading>Menu</Heading>
       </Container>
@@ -26,3 +36,18 @@ export function App() {
   )
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//o return nesse tipo de programação necessia de uma div "vazia" ("<> </>") para fazer todo o html dentro dela
