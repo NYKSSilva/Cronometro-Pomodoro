6e3ea4c0020221c4d0b5a191ac64c4560a6aba51
NYKSSilva <nicollysantossilva0508@gmail.com>
2026-03-11 09:16:48 -0300

    11/03

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,9 +1,11 @@
 import styles from './styles.module.css';
-import {TimerIcon} from './styles.module.css'
+import {TimerIcon} from 'lucide-react';
 export function Logo(){
    return (
-    <div>
-        <a href="#"><TimerIcon/><span>Teste</span></a>
+    <>
+    <div className={styles.logo}>
+        <a className={styles.logoLink} href="#"><TimerIcon/><span>Chronos</span></a>
     </div>
-   )
+    </>
+  )  
 }
