ddfdb6284cf5ab89cbd393db0a50e8f6f5431f78
NYKSSilva <nicollysantossilva0508@gmail.com>
2026-03-24 08:22:47 -0300

    24/03

--- src/components/Cycles/index.tsx
+++ src/components/Cycles/index.tsx
@@ -2,20 +2,26 @@ import styles from './style.module.css'
 
 export function Cycles(){
     return(
-        <> 
-        <div className={styles.cycles}>
-            <span>Ciclos</span>
-            <div className={styles.cycleDots}>
-                <span className={`${styles.cycleDots} ${styles.workTime}`}></span>
-                <span className={`${styles.cycleDots} ${styles.workTime}`}></span>
-                <span className={`${styles.cycleDots} ${styles.workTime}`}></span>
-                <span className={`${styles.cycleDots} ${styles.workTime}`}></span>
-                <span className={`${styles.cycleDots} ${styles.workTime}`}></span>
-                <span className={`${styles.cycleDots} ${styles.workTime}`}></span>
-                <span className={`${styles.cycleDots} ${styles.workTime}`}></span>
-
-            </div>
-        </div>
+          <>
+            <div className= {styles.cycles}>
+                <span>Ciclos: </span>    
+            </div> 
+            <div className= {styles.cycleDots}>
+                <span className = {`
+                    ${styles.cycleDot} ${styles.workTime}`}> </span>   
+                    <span className = {`
+                    ${styles.cycleDot} ${styles.workTime}`}> </span>    
+                    <span className = {`
+                    ${styles.cycleDot} ${styles.workTime}`}> </span>    
+                    <span className = {`
+                    ${styles.cycleDot} ${styles.workTime}`}> </span>    
+                    <span className = {`
+                    ${styles.cycleDot} ${styles.workTime}`}> </span>    
+                    <span className = {`
+                    ${styles.cycleDot} ${styles.workTime}`}> </span>    
+                    <span className = {`
+                    ${styles.cycleDot} ${styles.workTime}`}> </span>     
+            </div> 
         </>
     )
 }
--- src/components/DefaultInput/index.tsx
+++ src/components/DefaultInput/index.tsx
@@ -1,16 +1,22 @@
 import styles from './styles.module.css';
 type DefaultInputProps ={
-  id: string;
-  // type: string
-  labelText: string;
-}&React.Component<'input'>
-export function DefaultInput({id, type, labelText = "task:", ...rest}: DefaultInputProps){
-   return (
-    <>
-    <label htmlFor={id}>{labelText}</label>
-    <input className = {styles.input} id = {id} type= {type} {...rest}/>
-    </>
+    id: string;
+    labelText: string;
+    type: string
+}&React.Component<'input'>;
 
-    
-  )  
+export function DefaultInput({
+    id,
+    type,  
+    labelText = "task",
+    ...rest
+}: DefaultInputProps){
+    return(
+    <>
+        <div className= {styles.teste}> 
+            <label htmlFor = {id}> {labelText} </label>
+            <input className  = {styles.input} id = {id} type = {type} {...rest}/>
+        </div>
+    </>
+    )  
 }
